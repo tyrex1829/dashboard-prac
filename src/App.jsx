6a588aca430a56688bc03f9c14a39b5fc4e205cd
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-02 13:12:01 +0530

    starting dashboard project

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className=" bg-red-700">Red</div>
-        <div className=" bg-green-700">Green</div>
-        <div className=" bg-blue-700">Blue</div>
-      </div>
-    </>
-  );
+  // return (
+  //   <>
+  //     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+  //       <div className=" bg-red-700">Red</div>
+  //       <div className=" bg-green-700">Green</div>
+  //       <div className=" bg-blue-700">Blue</div>
+  //     </div>
+  //   </>
+  // );
 }
 
 export default App;
