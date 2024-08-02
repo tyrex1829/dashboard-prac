af045577398fec4d21855c199ce01e9a5b4e9804
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-02 14:16:08 +0530

    money components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import "./App.css";
+import RevenueCard from "./components/RevenueCard";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>vdv</div>
-    </>
+    <div className="grid grid-cols-4 gap-4">
+      <RevenueCard
+        title={"Next Payout"}
+        amount={"2,312.23"}
+        orderCount={"23"}
+      />
+      <RevenueCard
+        title={"Amount Pending"}
+        amount={"92,312.20"}
+        orderCount={"13"}
+      />
+      <RevenueCard
+        title={"Amount Processed"}
+        amount={"23,92,312.19"}
+        orderCount={""}
+      />
+    </div>
   );
 
   // for tailwind practice
---
+++ src/components/RevenueCard.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const RevenueCard = ({ title, orderCount, amount }) => {
+  return (
+    <div className="bg-white rounded shadow-sm p-6">
+      <div className="text-gray-700 flex">
+        <div className=" pr-2">{title}</div>
+        <div className="flex justify-center flex-col">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div className="flex justify-between pt-2">
+        <div className="text-2xl font-semibold">₹{amount}</div>
+        {orderCount ? (
+          <div className="flex cursor-pointer font-medium text-blue-700 underline flex flex-col justify-center">
+            <div className="flex justify-center">
+              <div>{orderCount} order(s) </div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default RevenueCard;
